9399b5f4e94c3c7c854ddd1b74608573001e169a
Yurii Kantonistov <yurii.kantonistov@nokia.com>
2025-10-23 23:00:15 +0300

    Store User to angu_user table in H2 DB.

--- anguboot/src/main/java/com/baeldung/application/entities/User.java
+++ anguboot/src/main/java/com/baeldung/application/entities/User.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entity
+@Entity(name="angu_users")
 public class User {
     
     @Id
