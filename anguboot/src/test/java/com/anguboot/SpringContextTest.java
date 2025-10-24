fb8f35c29e0bba9e890e65b3dc8736cc4c08fcb1
Yurii Kantonistov <yurii.kantonistov@nokia.com>
2025-10-24 20:56:16 +0300

    Streamlined test class names.

--- anguboot/src/test/java/com/anguboot/SpringContextTest.java
+++ anguboot/src/test/java/com/anguboot/SpringContextTest.java
@@ -1,11 +1,9 @@
-package com.baeldung;
+package com.anguboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.anguboot.Application;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
